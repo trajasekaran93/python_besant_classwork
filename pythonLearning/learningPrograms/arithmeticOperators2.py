3701a41be028557ace8d81ad666daa3daec13e7b
trajasekaran93 <trajasekaran93@gmail.com>
2024-09-09 20:09:12 +0530

    FirstCommit

---
+++ pythonLearning/learningPrograms/arithmeticOperators2.py
@@ -0,0 +1,24 @@
+
+
+print(10+2)
+print(10-2)
+print(10*2)
+print(10/3)     # division operator
+print(10//3)    # floor division - return the quotient o/p - 3
+print(10%6)     # modulus - return the remainder after division o/p - 4
+print("--------")
+
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**2)     # it means a*a (10*10)
+print(b**3)     # it means b*b*b (5*5*5)
+print("a value is :",a,"now")
+# print("a value is :"+a )     # throws error can't add string with int
+
+
---
+++ pythonLearning/learningPrograms/basic1.py
@@ -0,0 +1,22 @@
+
+
+print("Hello world")
+
+print ('Hello world')
+
+print (3+4)  # + operator will do additon operation when used in b/w two integer
+
+print("3"+"4")  # python consider as two string, + operator is act as concat two string value
+
+print("3+4")  # will print string value inside double quotes
+
+print("sun"+"rise")  # concat two separate sring value
+
+# print ("4"+2)  # throws error, python doesn't allow to concate one string and one int value
+
+print (2*"hello")  # it will print the string value two times, multiplication allow to print one int and one str value
+
+print(3*4)  # it will do multiplication operation
+
+# print("3"*"4")  # not allow to mulitple two string value , throws error
+
